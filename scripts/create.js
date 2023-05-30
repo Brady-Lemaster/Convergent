fcf7605728dc478292429154877959df35c192c5
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-29 21:46:07 -0500

    Update and rename external.js to create.js

---
+++ scripts/create.js
@@ -0,0 +1,9 @@
+function importTailwind() {
+  const tailwindDOM = document.createElement("script");
+  tailwindDOM.src = "";
+  document.body.appendChild(tailwindDOM);
+}
+
+containingElement.innerHTML = "<div id="divA"><div id="divB">Some Text"</div></div>";
+
+tailwind()
