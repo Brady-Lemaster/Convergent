4f62c5c6c2318c5a3b4144e56dd61e9f5e06e4c1
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-29 21:56:53 -0500

    Update create.js

--- scripts/create.js
+++ scripts/create.js
@@ -1,7 +1,24 @@
+function createMeta() {
+  const metaDOM = document.createElement("meta");
+  metaDOM.charset = "utf-8";
+  metaDOM.name = "viewport";
+  metaDOM.content = "width=device-width, initial-scale=1";
+  document.body.appendChild(metaDOM);  
+}
+
 function importTailwind() {
   const tailwindDOM = document.createElement("script");
   tailwindDOM.src = "https://cdn.tailwindcss.com";
   document.body.appendChild(tailwindDOM);
 }
+  
+function createHead() {
+  createMeta();
+}
 
-importTailwind()
+function importExternal() {
+  importTailwind();
+}
+  
+createHead();
+importExternal();
